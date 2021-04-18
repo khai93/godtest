9c2890a7525d4475ccee4da560bcf6c6376df911
khai93 <33293519+khai93@users.noreply.github.com>
2021-04-18 01:32:19 -0500

    add danny

---
+++ index.ts
@@ -0,0 +1,88 @@
+import { Logger } from 'tslog';
+import { ILoggerModule, TsLogLoggerModuleDependencies, NodeLoggerModule, TsLogLoggerModule } from './logger';
+
+export type DannyDependencies = {
+    logger: ILoggerModule
+}
+
+export interface IValidatable {
+    isValid(): boolean
+}
+
+
+class UsernameNotValidError extends Error {
+    constructor(message) {
+        super(message);
+
+        // assign the error class name in your custom error (as a shortcut)
+        this.name = this.constructor.name;
+
+        // capturing the stack trace keeps the reference to your error class
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
+class PasswordNotValidError extends Error {
+    constructor(message) {
+        super(message);
+
+        // assign the error class name in your custom error (as a shortcut)
+        this.name = this.constructor.name;
+
+        // capturing the stack trace keeps the reference to your error class
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export class Username implements IValidatable {
+    private _username: string;
+
+    constructor(username: string) {
+        this._username = username;
+
+        if (!this.isValid()) {
+            throw new UsernameNotValidError("Username has less than 3 characters");
+        }
+    }
+
+    isValid = () => this._username.length >= 3;
+}
+
+export class Password implements IValidatable {
+    private _password: string;
+
+    constructor(username: string) {
+        this._password = username;
+
+        if (!this.isValid()) {
+            throw new PasswordNotValidError("Password has less than 3 characters");
+        }
+    }
+
+    isValid = () => this._password.length >= 3;
+}
+
+class Danny {
+    constructor(private dependencies: DannyDependencies) {
+        this.dependencies.logger.log("I AM DANNY");
+    }
+
+    register(username: Username, password: Password) {
+        this.dependencies.logger.log("Registered to Roblox.com");
+    }
+
+    scream(error: Error) {
+        this.dependencies.logger.error(error);
+    }
+}
+
+const LoggerModule = new TsLogLoggerModule({tsLogLogger: new Logger({})});
+
+const DannyInstance = new Danny({logger: LoggerModule});
+
+try {
+    DannyInstance.register(new Username("dng"), new Password("12"));
+} catch (e) {
+    DannyInstance.scream(e);
+}
---
+++ logger.ts
@@ -0,0 +1,44 @@
+import { ILogObject, Logger, LoggerWithoutCallSite } from "tslog";
+
+LoggerWithoutCallSite
+export interface ILoggerModule {
+    log(message: string);
+    warn(message: string);
+    error(error: Error);
+}
+
+
+export class NodeLoggerModule implements ILoggerModule {
+    log(message: string) {
+        console.log(message);
+    }
+
+    warn(message: string) {
+        console.warn(message);
+    }
+
+    error(error: Error) {
+        console.error(error.message);
+    }
+}
+
+export type TsLogLoggerModuleDependencies = {
+    tsLogLogger: Logger
+}
+
+export class TsLogLoggerModule implements ILoggerModule {
+    
+    constructor(private dependencies: TsLogLoggerModuleDependencies) {}
+
+    log(message: string) {
+        this.dependencies.tsLogLogger.silly(message);
+    }
+    warn(message: string) {
+        this.dependencies.tsLogLogger.warn(message);
+    }
+    error(error: Error) {
+        this.dependencies.tsLogLogger.prettyError(error);
+    }
+
+}
+
